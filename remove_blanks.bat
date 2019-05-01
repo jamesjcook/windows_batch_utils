8c91c85fe0006798172a7a2ff3dffae08d6e500b
James Cook <jamesjcook@users.noreply.github.com>
2019-05-01 13:22:55 -0400

    Added different blank handling.

--- remove_blanks.bat
+++ remove_blanks.bat
@@ -1,13 +1,38 @@
-@echo off
 @REM remove blanks
 @echo off 
-setlocal enabledelayedexpansion 
+endlocal
 set source=%1
 set dest=%2
+set "clean=false"
+if [%2]==[] ( 
+  set "dest=%source%.clean"
+  set "clean=true"
+)
 
-(for /f "delims=" %%a in ('more +1 %source% ^| find /v ""') do (
-  set line=%%~a
-  set line=!line:"=!
-  set line=!line:,=!
-  echo !line!
-)) > %dest%
+@REM remove any trailing whitespace.
+@REM side effect of removing exclaimation punctuation.
+
+@REM @echo off > %dest% & setLocal enableDELAYedeXpansioN
+@REM for /f "tokens=1* delims=]" %%a in ('find /N /V "" ^<%source%') do (
+@REM     SET "str=%%b"
+@REM     for /l %%i in (1,1,31) do if "!str:~-1!"==" " set "str=!str:~0,-1!"
+@REM     >>%dest% SET /P "l=!str!"<nul
+@REM     >>%dest% echo.
+@REM )
+@REM echo "trailing whitespace removed.
+@REM timeout 30
+@REM if %clean%==true (
+@REM   del %source%
+@REM   move %dest% %source%
+@REM )
+
+@REM remove any completely blank lines
+for /f "usebackq tokens=* delims=" %%a in ("%source%") do (echo(%%a)>>%dest%
+
+if %clean%==true (
+  del %source%
+  move %dest% %source%
+)
+
+
+exit /b 
